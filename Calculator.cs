6d936435e9849ac378167fd343a75519eceae9d3
jeff <163700716+Slaterfr@users.noreply.github.com>
2025-02-03 19:03:40 -0600

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        double Numero1 = 0, Numero2 = 0;
+        char Operador;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AgregarNumero(object sender, EventArgs e)
+        {
+            var Boton = ((Button)sender);
+            if (txtResultado.Text == "0")
+                txtResultado.Text = "";
+            txtResultado.Text += Boton.Text;
+        }
+        private void Borrar(object sender, EventArgs e) {
+            var Boton = ((Button)sender);
+            if (txtResultado.Text != "0")
+                txtResultado.Text = "0";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            Numero2 = Convert.ToDouble(txtResultado.Text);
+
+            if (Operador == '+')
+            {
+                txtResultado.Text = (Numero1 + Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+
+            }
+            else if (Operador == '-') {
+                txtResultado.Text = (Numero1 - Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            else if (Operador == 'X')
+            {
+                txtResultado.Text = (Numero1 * Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            else if (Operador == '/')
+            {
+                if (txtResultado.Text != "0")
+                {
+                    txtResultado.Text = (Numero1 / Numero2).ToString();
+                    Numero1 = Convert.ToDouble(txtResultado.Text);
+                }
+                else {
+                    MessageBox.Show("!No se puede dividir entre cero!");
+                }
+            }
+
+        }
+
+        private void btnBorrarDigito_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text.Length > 1)
+            {
+                txtResultado.Text = txtResultado.Text.Substring(0, txtResultado.Text.Length - 1);
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
+        }
+
+        private void btnBorrarTodo_Click(object sender, EventArgs e)
+        {
+            Numero1 = 0;
+            Numero2 = 0;
+            Operador = '\0';
+            txtResultado.Text = "0";
+        }
+
+        private void btnDecimal_Click(object sender, EventArgs e)
+        {
+            if (!txtResultado.Text.Contains(","))
+            {
+                txtResultado.Text += ",";
+            }
+        }
+
+        private void btnCambiarSigno_Click(object sender, EventArgs e)
+        {
+            Numero1 = Convert.ToDouble(txtResultado.Text);
+            Numero1 *= -1;
+            txtResultado.Text = Numero1.ToString();
+        }
+
+        private void clickOperador(object sender, EventArgs e)
+        {
+            var Boton = ((Button)sender);
+            Numero1 = Convert.ToDouble(txtResultado.Text);
+            Operador = Convert.ToChar(Boton.Tag);
+            if (Operador == '²')
+            {
+                Numero1 = Math.Pow(Numero1, 2);
+                txtResultado.Text = Numero1.ToString();
+            }
+
+            else if (Operador == '√')
+            {
+                Numero1 = Math.Sqrt(Numero1);
+                txtResultado.Text = Numero1.ToString();
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
+        }
+
+    }
+}
